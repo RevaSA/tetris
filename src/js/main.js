eb7c5332de89dba64dc0de4680be6eedf7d233ce
Stanislav Reva <RevaSA95@gmail.com>
2020-11-25 15:43:24 +0300

    Add empty board

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-console.log('Hello World!')
+import { ROWS, COLS, BLOCK_SIZE } from './constants';
+
+const canvas = document.getElementById('board');
+const ctx = canvas.getContext('2d');
+
+// Устанавливаем размеры холста
+ctx.canvas.width = COLS * BLOCK_SIZE;
+ctx.canvas.height = ROWS * BLOCK_SIZE;
+
+// Устанавливаем масштаб
+ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
